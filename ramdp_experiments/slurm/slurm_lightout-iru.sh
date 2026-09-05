727c184354f1079cf2c7f7d01e59f4cc1cabd6df
Bryan Chan <bryan.chan@ualberta.ca>
2026-09-05 06:02:13 -0600

    update

--- ramdp_experiments/slurm/slurm_lightout-iru.sh
+++ ramdp_experiments/slurm/slurm_lightout-iru.sh
@@ -15,9 +15,9 @@ cd /home/chanb/research/iclr_2027/Stoix
 echo "hostname: $(hostname)"
 echo "starting at: $(date)"
 
-python ramdp_experiments/lightsout_fixed_budget_sweep.py --systems ff_ppo_reinforce --budget 1,2,4,8,16 --seeds 5 --architectures iru --hidden-dim 64,128 --num-layers 1 --total-timesteps 1e8 --clip-value-loss false --lr 3e-4 --critic-lr 3e-4 --epochs 4 --num-minibatches 16 --grid-sizes 4x5 --use-input-layer-norm true --episode-length 10 --wandb true --wandb-project lightsout-iru-sep5 --runs-per-gpu 4 --gpus 0 --no-skip-existing --ent-coef 0.001 --gamma 0.999 --server vulcan --actor-weight-decay 0.01 --yes &
+python ramdp_experiments/lightsout_fixed_budget_sweep.py --systems ff_ppo_reinforce --budget 1,2,4,8,16 --seeds 5 --architectures iru --hidden-dim 32 --num-layers 1 --total-timesteps 1e8 --clip-value-loss false --lr 3e-4 --critic-lr 3e-4 --epochs 4 --num-minibatches 16 --grid-sizes 4x5 --use-input-layer-norm true --episode-length 10 --wandb true --wandb-project lightsout-iru-sep5-recompute_adv --runs-per-gpu 4 --gpus 0 --no-skip-existing --ent-coef 0.001 --gamma 0.999 --server vulcan --actor-weight-decay 0.01 --yes &
 
-python ramdp_experiments/lightsout_sweep.py --systems ff_ppo_cond_fac,ff_ppo_cond_naive,ff_ppo_reinforce --max-steps 16 --seeds 5 --architectures iru --hidden-dim 64,128 --num-layers 1 --total-timesteps 1e8 --clip-value-loss false --lr 3e-4 --critic-lr 1e-5,3e-4 --epochs 4 --num-minibatches 16 --grid-sizes 4x5 --use-input-layer-norm true --episode-length 10 --wandb true --wandb-project lightsout-iru-sep5 --runs-per-gpu 4 --gpus 1 --no-skip-existing --ent-coef 0.001 --gamma 0.999 --server vulcan --actor-weight-decay 0.01 --yes &
+python ramdp_experiments/lightsout_sweep.py --systems ff_ppo_cond_fac,ff_ppo_cond_naive,ff_ppo_reinforce --max-steps 16 --seeds 5 --architectures iru --hidden-dim 32 --num-layers 1 --total-timesteps 1e8 --clip-value-loss false --lr 3e-4 --critic-lr 1e-5,3e-4 --epochs 4 --num-minibatches 16 --grid-sizes 4x5 --use-input-layer-norm true --episode-length 10 --wandb true --wandb-project lightsout-iru-sep5-recompute_adv --runs-per-gpu 4 --gpus 1 --no-skip-existing --ent-coef 0.001 --gamma 0.999 --server vulcan --actor-weight-decay 0.01 --yes &
 
 wait
 
