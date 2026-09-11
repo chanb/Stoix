33a82b4b4050eda893a42ce53444f32c619dab74
Bryan Chan <bryan.chan@ualberta.ca>
2026-09-11 07:02:11 -0600

    update scripts

--- ramdp_experiments/slurm/lightsout-ecot.sh
+++ ramdp_experiments/slurm/lightsout-ecot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=aip-schuurma
-#SBATCH --time=11:59:00
+#SBATCH --time=02:59:00
 #SBATCH --mem=32GB
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:2
@@ -19,11 +19,13 @@ cd /home/chanb/research/iclr_2027/Stoix
 echo "hostname: $(hostname)"
 echo "starting at: $(date)"
 
+project_name=sep11
+vocab_size=1
 
-# python ramdp_experiments/lightsout_fixed_budget_sweep.py --systems ff_ppo_explicit_reinforce --budget 1 --seeds 30 --architectures transformer_explicit_cot --hidden-dim 64 --mlp-dim 128 --num-layers 4 --num-heads 8 --vocab-size=1 --total-timesteps 3e8 --clip-value-loss false --lr 3e-4 --critic-lr 3e-4 --epochs 4 --num-minibatches 16 --grid-sizes 5x5 --use-input-layer-norm false --episode-length 10 --wandb true --wandb-project lightsout-sep8 --runs-per-gpu 5 --gpus 0,1 --no-skip-existing --ent-coef 0.001 --gamma 0.99 --server vulcan --actor-weight-decay 0.01 --recompute-advantages false --critic-before-actor false --yes
+# python ramdp_experiments/lightsout_fixed_budget_sweep.py --systems ff_ppo_explicit_reinforce --budget 1,2,3 --seeds 10 --architectures transformer_explicit_cot --hidden-dim 64 --mlp-dim 128 --num-layers 4 --num-heads 8 --vocab-size=${vocab_size} --total-timesteps 3e8 --clip-value-loss false --lr 3e-4 --critic-lr 3e-4 --epochs 4 --num-minibatches 16 --grid-sizes 5x5 --use-input-layer-norm false --episode-length 10 --ent-coef 0.001 --gamma 0.99 --actor-weight-decay 0.01 --gae-lambda 0.95  --standardize-advantages true --wandb true --wandb-project lightsout-${project_name} --output-dir /home/chanb/scratch/logs/ramdp/lightsout-ecot-${project_name} --runs-per-gpu 4 --gpus 0,1 --no-skip-existing --server vulcan --yes
 
-# python ramdp_experiments/lightsout_fixed_budget_sweep.py --systems ff_ppo_explicit_reinforce --budget 2,3,4,5 --seeds 30 --architectures transformer_explicit_cot --hidden-dim 64 --mlp-dim 128 --num-layers 4 --num-heads 8 --vocab-size=1,2,4,8,16 --total-timesteps 3e8 --clip-value-loss false --lr 3e-4 --critic-lr 3e-4 --epochs 4 --num-minibatches 16 --grid-sizes 5x5 --use-input-layer-norm false --episode-length 10 --wandb true --wandb-project lightsout-sep8 --runs-per-gpu 5 --gpus 0,1 --no-skip-existing --ent-coef 0.001 --gamma 0.99 --server vulcan --actor-weight-decay 0.01 --recompute-advantages false --critic-before-actor false --yes
+# python ramdp_experiments/lightsout_fixed_budget_sweep.py --systems ff_ppo_explicit_reinforce --budget 4,5 --seeds 10 --architectures transformer_explicit_cot --hidden-dim 64 --mlp-dim 128 --num-layers 4 --num-heads 8 --vocab-size=${vocab_size} --total-timesteps 3e8 --clip-value-loss false --lr 3e-4 --critic-lr 3e-4 --epochs 4 --num-minibatches 16 --grid-sizes 5x5 --use-input-layer-norm false --episode-length 10 --ent-coef 0.001 --gamma 0.99 --actor-weight-decay 0.01 --gae-lambda 0.95  --standardize-advantages true --wandb true --wandb-project lightsout-${project_name} --output-dir /home/chanb/scratch/logs/ramdp/lightsout-ecot-${project_name} --runs-per-gpu 4 --gpus 0,1 --no-skip-existing --server vulcan --yes
 
-# python ramdp_experiments/lightsout_sweep.py --systems ff_ppo_explicit_cond_fac,ff_ppo_explicit_cond_naive,ff_ppo_explicit_reinforce --max-steps 5 --seeds 30 --architectures transformer_explicit_cot --hidden-dim 64 --mlp-dim 128 --num-layers 4 --num-heads 8 --vocab-size=1,2,4,8,16 --total-timesteps 3e8 --clip-value-loss false --lr 3e-4 --critic-lr 3e-4 --epochs 4 --num-minibatches 16 --grid-sizes 5x5 --use-input-layer-norm false --episode-length 10 --wandb true --wandb-project lightsout-sep8 --runs-per-gpu 5 --gpus 0,1 --no-skip-existing --ent-coef 0.001 --gamma 0.99 --server vulcan --actor-weight-decay 0.01 --recompute-advantages false --critic-before-actor false --yes
+python ramdp_experiments/lightsout_sweep.py --systems ff_ppo_explicit_reinforce --max-steps 5 --seeds 10 --architectures transformer_explicit_cot --hidden-dim 64 --mlp-dim 128 --num-layers 4 --num-heads 8 --vocab-size=${vocab_size} --total-timesteps 3e8 --clip-value-loss false --lr 3e-4 --critic-lr 3e-4 --epochs 4 --num-minibatches 16 --grid-sizes 5x5 --use-input-layer-norm false --episode-length 10 --ent-coef 0.001 --gamma 0.99 --actor-weight-decay 0.01 --gae-lambda 0.95  --standardize-advantages true --wandb true --wandb-project lightsout-${project_name} --output-dir /home/chanb/scratch/logs/ramdp/lightsout-ecot-${project_name} --runs-per-gpu 4 --gpus 0,1 --no-skip-existing --server vulcan --yes
 
 echo "finished with exit code $? at: $(date)"
--- ramdp_experiments/slurm/maze-iru.sh
+++ ramdp_experiments/slurm/maze-iru.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=aip-schuurma
-#SBATCH --time=23:59:00
+#SBATCH --time=11:59:00
 #SBATCH --mem=32GB
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:2
@@ -19,10 +19,13 @@ cd /home/chanb/research/iclr_2027/Stoix
 echo "hostname: $(hostname)"
 echo "starting at: $(date)"
 
+project_name=sep11
 
-# python ramdp_experiments/jumanji_fixed_budget_sweep.py --systems ff_ppo_reinforce --budget 1,2,3,4,5 --seeds 5 --runs-per-gpu 4 --architectures cnn+iru --hidden-dim 64 --num-layers 1 --total-timesteps 1e8 --clip-value-loss false --lr 3e-4 --critic-lr 3e-4 --envs maze --total-num-envs 128 --maze-size 10 --gpus 0,1 --rollout-length 10 --epochs 2 --num-minibatches 2 --ent-coef 0.01 --gamma 0.99 --actor-weight-decay 0.0 --critic-before-actor false --qv-critic separate --use-input-layer-norm false --standardize-advantages true --wandb true --wandb-project maze-sep10 --yes --no-skip-existing --server vulcan
+python ramdp_experiments/jumanji_fixed_budget_sweep.py --systems ff_ppo_reinforce --budget 1,2,3 --seeds 10 --runs-per-gpu 4 --architectures cnn+iru --hidden-dim 128 --num-layers 2 --total-timesteps 1e8 --clip-value-loss false --lr 3e-4 --critic-lr 3e-4 --envs maze --total-num-envs 128 --maze-size 10 --gpus 0,1 --rollout-length 10 --epochs 2 --num-minibatches 2 --ent-coef 0.01 --gamma 0.99 --use-input-layer-norm true --standardize-advantages true --gae-lambda 0.95 --wandb true --wandb-project maze-${project} --output-dir /home/chanb/scratch/logs/ramdp/maze-iru-${project_name} --yes --no-skip-existing --server vulcan
 
-python ramdp_experiments/jumanji_sweep.py --systems ff_ppo_cond_fac,ff_ppo_cond_naive,ff_ppo_reinforce --max-steps 5 --seeds 5 --runs-per-gpu 3 --architectures cnn+iru --hidden-dim 64 --num-layers 1 --total-timesteps 1e8 --clip-value-loss false --lr 3e-4 --critic-lr 3e-4 --envs maze --total-num-envs 128 --maze-size 10 --gpus 0,1 --rollout-length 10 --epochs 2 --num-minibatches 2 --ent-coef 0.01 --gamma 0.99 --actor-weight-decay 0.0 --critic-before-actor false --qv-critic separate --use-input-layer-norm false --standardize-advantages true --wandb true --wandb-project maze-sep10 --yes --no-skip-existing --server vulcan
+python ramdp_experiments/jumanji_fixed_budget_sweep.py --systems ff_ppo_reinforce --budget 4,5 --seeds 10 --runs-per-gpu 4 --architectures cnn+iru --hidden-dim 128 --num-layers 2 --total-timesteps 1e8 --clip-value-loss false --lr 3e-4 --critic-lr 3e-4 --envs maze --total-num-envs 128 --maze-size 10 --gpus 0,1 --rollout-length 10 --epochs 2 --num-minibatches 2 --ent-coef 0.01 --gamma 0.99 --use-input-layer-norm true --standardize-advantages true --gae-lambda 0.95 --wandb true --wandb-project maze-${project} --output-dir /home/chanb/scratch/logs/ramdp/maze-iru-${project_name} --yes --no-skip-existing --server vulcan
+
+python ramdp_experiments/jumanji_sweep.py --systems ff_ppo_reinforce --max-steps 5 --seeds 10 --runs-per-gpu 3 --architectures cnn+iru --hidden-dim 128 --num-layers 2 --total-timesteps 1e8 --clip-value-loss false --lr 3e-4 --critic-lr 3e-4 --envs maze --total-num-envs 128 --maze-size 10 --gpus 0,1 --rollout-length 10 --epochs 2 --num-minibatches 2 --ent-coef 0.01 --gamma 0.99 --use-input-layer-norm true --standardize-advantages true --gae-lambda 0.95 --wandb true --wandb-project maze-${project} --output-dir /home/chanb/scratch/logs/ramdp/maze-iru-${project_name} --yes --no-skip-existing --server vulcan
 
 wait
 
