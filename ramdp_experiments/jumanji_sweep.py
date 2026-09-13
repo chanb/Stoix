91727cdfcf3cceb197f706e427f4c4ee2627eab5
Bryan Chan <bryan.chan@ualberta.ca>
2026-09-13 14:14:25 -0600

    CNN for cokoban

--- ramdp_experiments/jumanji_sweep.py
+++ ramdp_experiments/jumanji_sweep.py
@@ -453,10 +453,18 @@ ENV_CNN_ARCH = {
     #     "critic_hidden_sizes": (128,),
     #     "critic_layer_sizes": (128, 128),
     # },
-    "sokoban": { # sep12
-        "channel_sizes": (64, 64),
-        "kernel_sizes": (3, 3),
-        "strides": (2, 1),
+    # "sokoban": { # sep12
+    #     "channel_sizes": (64, 64),
+    #     "kernel_sizes": (3, 3),
+    #     "strides": (2, 1),
+    #     "hidden_sizes": (128,),
+    #     "critic_hidden_sizes": (128,),
+    #     "critic_layer_sizes": (128, 128),
+    # },
+    "sokoban": {
+        "channel_sizes": (64,),
+        "kernel_sizes": (3,),
+        "strides": (2,),
         "hidden_sizes": (128,),
         "critic_hidden_sizes": (128,),
         "critic_layer_sizes": (128, 128),
